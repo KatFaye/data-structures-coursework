2ca1b5e89781a67992f86f599a01383ddbc88320
kherring <kherring@nd.edu>
2016-11-28 15:41:34 -0500

    completed challenge11

--- challenge11/map.cpp
+++ challenge11/map.cpp
@@ -1,8 +1,32 @@
 // map.cpp: inverted index
 
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
+	string alnum = "abcdefghijklmnopqrstuvwxyz0123456789";
+	int line = 0;
+	string input;
+	while(getline(cin, input)) {
+		transform(input.begin(), input.end(), input.begin(), ::tolower);
+		stringstream ssinp(input);
+
+		string word;
+		line++;
+		while(ssinp >> word) { //iterate over words
+			int first;
+			int last;
+			first = word.find_first_of(alnum);
+			last = word.find_last_of(alnum);
+			if(last-first+1 > 0 && first != -1) //characters were found and size is not 0
+				cout << word.substr(first, last+1-first) << "\t" << line << endl;
+		}
+	}
     return EXIT_SUCCESS;
 }
 
--- challenge11/reduce.cpp
+++ challenge11/reduce.cpp
@@ -1,8 +1,36 @@
 // reduce.cpp: inverted index
 
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+#include <algorithm>
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
+	map<string, set<int>> dictionary;
+	string input;
+	int line;
+	while(cin >> input) {
+		cin >> line;
+		dictionary[input].insert(line);
+		
+	}
+	for(auto it = dictionary.begin(); it!=dictionary.end(); it++) {
+		cout << it->first << "\t";
+		bool first = true;
+		for(auto s = it->second.begin(); s!=it->second.end(); s++) {
+			if(!first) {
+				cout << " " << *s;
+			}	else {
+				cout << *s;
+			}
+			first = false;
+		}
+		cout << endl;
+	}
     return EXIT_SUCCESS;
 }
 
